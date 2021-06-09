64eb78cd0be84457bfef7c83e63d28872e6e8112
CarlosBenetti <benetticarlos1@gmail.com>
2021-06-09 15:11:38 -0300

    subida de archivos

---
+++ index.js
@@ -0,0 +1,48 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 1500,
+        origin: 'bottom'
+    });
+
+    sr.reveal('.imagenPerfil', {
+        duration: 2000,
+        origin: 'top',
+        distance: '100px'
+    });
+
+    sr.reveal('#presentacion', {
+        duration: 2000,
+        origin: 'right',
+        distance: '100px'
+    });
+
+
+    // sr.reveal('.container-hijo', {
+    //     duration: 2000,
+    //     origin: 'bottom',
+    //     distance: '100px'
+    // });
+
+    sr.reveal('#WhoAmI', {
+        duration: 2000,
+        origin: 'left',
+        distance: '100px'
+    });
+
+    sr.reveal('#skills', {
+        duration: 3500
+    });
+
+    sr.reveal('#contactMe', {
+        duration: 2000
+    })
+
+
+
+    sr.reveal('#portfolioCards', {
+        duration: 3000
+    })
+
+
+    
