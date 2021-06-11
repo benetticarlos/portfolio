8a6d68b007972aaf3694558408679005efbb665c
CarlosBenetti <benetticarlos1@gmail.com>
2021-06-10 21:08:41 -0300

    detalles en vista mobile y animaciones
    termine de pulir la vista mobile y ajuste los tiempos de las animaciones

--- animation.js
+++ animation.js
@@ -2,37 +2,44 @@ window.sr = ScrollReveal();
 
     sr.reveal('.navbar', {
         duration: 1500,
-        origin: 'bottom'
     });
 
     sr.reveal('.imagenPerfil', {
-        duration: 2000,
-        origin: 'top',
-        distance: '100px'
+        duration: 3000
+        // origin: 'top',
+        // distance: '100px'
     });
 
     sr.reveal('#presentacion', {
-        duration: 2000,
-        origin: 'right',
-        distance: '100px'
+        duration: 3000
+        // origin: 'right',
+        // distance: '100px'
     });
 
 
-    sr.reveal('#WhoAmI', {
-        duration: 2000,
-        // origin: 'left',
-        distance: '100px'
+
+
+    sr.reveal('#about', {
+        duration: 2500
     });
 
-    sr.reveal('#skills', {
-        duration: 3500
-    });
 
-    sr.reveal('#contactMe', {
-        duration: 2000
+
+    sr.reveal('#portfolio', {
+        duration: 2500
+    })
+
+    sr.reveal('#contact', {
+        duration: 2500
     })
 
 
     // sr.reveal('#portfolioCards', {
     //     duration: 3000
     // })
+
+    // sr.reveal('#WhoAmI', {
+    //     duration: 2000,
+    //     // origin: 'left',
+    //     distance: '10px'
+    // // });
