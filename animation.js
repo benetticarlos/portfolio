a7be3b0379a3cc96928564b9197eb8f2dc8ed2da
CarlosBenetti <benetticarlos1@gmail.com>
2021-06-10 21:53:21 -0300

    Codigo limpio
    limpieza de codigo

--- animation.js
+++ animation.js
@@ -1,45 +1,25 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.navbar', {
-        duration: 1500,
-    });
+sr.reveal(".navbar", {
+  duration: 1500,
+});
 
-    sr.reveal('.imagenPerfil', {
-        duration: 3000
-        // origin: 'top',
-        // distance: '100px'
-    });
+sr.reveal(".imagenPerfil", {
+  duration: 3000
+});
 
-    sr.reveal('#presentacion', {
-        duration: 3000
-        // origin: 'right',
-        // distance: '100px'
-    });
+sr.reveal("#presentacion", {
+  duration: 3000
+});
 
+sr.reveal("#about", {
+  duration: 2500
+});
 
+sr.reveal("#portfolio", {
+  duration: 2500
+});
 
-
-    sr.reveal('#about', {
-        duration: 2500
-    });
-
-
-
-    sr.reveal('#portfolio', {
-        duration: 2500
-    })
-
-    sr.reveal('#contact', {
-        duration: 2500
-    })
-
-
-    // sr.reveal('#portfolioCards', {
-    //     duration: 3000
-    // })
-
-    // sr.reveal('#WhoAmI', {
-    //     duration: 2000,
-    //     // origin: 'left',
-    //     distance: '10px'
-    // // });
+sr.reveal("#contact", {
+  duration: 2500
+});
